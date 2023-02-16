f22f75ac3065301ed6d1aa84e11a7c266a7caf73
dritchings <dritchings@qa.com>
2023-02-16 13:59:57 +0000

    fixed and moved org file

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -113,3 +113,11 @@
 (package! flx-rs)
 (package! fussy)
 ;; (package! straight)
+
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor"))
